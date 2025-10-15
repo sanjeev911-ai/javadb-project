485e031b25985eedcac934269cabec2f867eb85d
sanjeev911-ai <sanjeevps895@gmail.com>
2025-10-15 09:08:57 +0530

    Add files via upload

---
+++ student-CRUD/src/main/java/com/example/studentcrud/entity/student.java
@@ -0,0 +1,42 @@
+package com.example.studentcrud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class student {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+private String name;
+private String Email;
+public student() {
+	 
+}
+public student(String name,String email) {
+	 this.name=name;
+	 this.email=email;
+}
+public Long getId() {
+	 return id;
+}
+public void setId(Long id) {
+	 this.id = id;
+}
+public String getName() {
+	 return name;
+}
+public void setName(String name) {
+	 this.name = name;
+}
+public String getEmail() {
+	 return Email;
+}
+public void setEmail(String email) {
+	 Email = email;
+}
+
+
+}
